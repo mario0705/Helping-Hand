beec57a55c2dc850f0f2a429a27549cc30c6d065
vrajpatel2312 <vraj.vup@gmail.com>
2018-07-05 00:44:19 -0400

    1.

--- index.php
+++ index.php
@@ -23,7 +23,79 @@ include("head.php")
         <div class="card card-cascade wider reverse my-4 pb-5">
             <!--Card image-->
             <div class="view overlay rgba-white-slight wow fadeIn" style="visibility: visible; animation-name: fadeIn;">
-                <img src="2/1.jpg" class="img-fluid">
+                
+<link rel="stylesheet" href="http://idangero.us/swiper/dist/css/swiper.min.css">
+
+<style>
+    
+    .swiper-container {
+      width: 100%;
+      height: 100%;
+
+    }
+    .swiper-slide {
+      text-align: center;
+      font-size: 18px;
+      background: #fff;
+
+      /* Center slide text vertically */
+      display: -webkit-box;
+      display: -ms-flexbox;
+      display: -webkit-flex;
+      display: flex;
+      -webkit-box-pack: center;
+      -ms-flex-pack: center;
+      -webkit-justify-content: center;
+      justify-content: center;
+      -webkit-box-align: center;
+      -ms-flex-align: center;
+      -webkit-align-items: center;
+      align-items: center;
+    }
+  </style>
+</head>
+<body>
+
+<div class="swiper-container">
+<div class="swiper-wrapper">
+<div class="swiper-slide"><img src="./assets/image/1.jpg"></div>
+<div class="swiper-slide"><img src="./assets/image/1.jpg"></div>
+<div class="swiper-slide"><img src="./assets/image/1.jpg"></div>
+<div class="swiper-slide"><img src="./assets/image/1.jpg"></div>
+<div class="swiper-slide"><img src="./assets/image/1.jpg"></div>
+<div class="swiper-slide"><img src="./assets/image/1.jpg"></div>
+<div class="swiper-slide"><img src="./assets/image/1.jpg"></div>
+<div class="swiper-slide"><img src="./assets/image/1.jpg"></div>
+<div class="swiper-slide"><img src="./assets/image/1.jpg"></div>
+<div class="swiper-slide"><img src="./assets/image/1.jpg"></div>
+</div>
+
+<div class="swiper-pagination"></div>
+
+<div class="swiper-button-next"></div>
+<div class="swiper-button-prev"></div>
+</div>
+
+<script src="http://idangero.us/swiper/dist/js/swiper.min.js"></script>
+
+<script>
+    var swiper = new Swiper('.swiper-container', {
+      spaceBetween: 30,
+      centeredSlides: true,
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
+  </script>
                 <a href="#!">
                     <div class="mask waves-effect waves-light"></div>
                 </a>
