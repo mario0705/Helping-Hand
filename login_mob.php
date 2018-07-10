57c4b60ec60e0931bad84cfc6a5fb0a14e764e4b
vrajpatel2312 <vraj.vup@gmail.com>
2018-07-10 15:12:12 -0400

    ..

--- login_mob.php
+++ login_mob.php
@@ -14,11 +14,11 @@ $stmt->fetch();
 ?>
 <?php
  // Our custom secure way of starting a PHP session.
- if ($_SERVER["REQUEST_METHOD"] == "POST") {
+ 
 if (isset($_POST['email'], $_POST['p'])) {
     $email = $_POST['email'];
     $password = $_POST['p']; // The hashed password.
- 
+    
     if (login($email, $password, $mysqli) == true) {
         $stmt = $mysqli->prepare("SELECT teamname , email,mobile, gender, age FROM members where email='$email'");
                 while ( $stmt-> fetch() ) {
@@ -43,14 +43,16 @@ if (isset($_POST['email'], $_POST['p'])) {
         
         
     } else {
+      echo $email;
+      echo "email here";
         // Login failed 
-        header('Location: ../index.php?error=1');
+         //header('Location: ../index.php?error=1');
     }
 } else {
     // The correct POST variables were not sent to this page. 
     echo 'Invalid Request';
 }
-}
+
 ?>
 <?php
 include("head.php")
