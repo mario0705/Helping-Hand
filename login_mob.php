40e603def84e167fafa3024d434447aa6f546b3f
vrajpatel2312 <vraj.vup@gmail.com>
2018-07-11 11:26:36 -0400

    mob

--- login_mob.php
+++ login_mob.php
@@ -1,170 +1,51 @@
 <?php
-include_once 'includes/db_connect.php';
-include_once 'includes/functions.php';
-sec_session_start();
-if (login_check($mysqli) == true) {
-$logged = 'in';
-} else {
-$logged = 'out';
-}
-$stmt = $mysqli->prepare("SELECT round FROM round WHERE id ='0'");
-$stmt->execute();
-$stmt->bind_result($round);
-$stmt->fetch();
-?>
-<?php
- // Our custom secure way of starting a PHP session.
- if ($_SERVER["REQUEST_METHOD"] == "POST"){
-if (isset($_POST['email'], $_POST['p'])) {
+include_once 'db_connect.php';
+include_once 'functions.php';
+ 
+sec_session_start(); // Our custom secure way of starting a PHP session.
+ 
+if (isset($_POST['email'], $_POST['pass'])) {
     $email = $_POST['email'];
-    $password = $_POST['p']; // The hashed password.
-    
+    $pass = $_POST['pass'];
+    $password = hash('sha512', $pass);
+    // The hashed password.
+ 
     if (login($email, $password, $mysqli) == true) {
-        $stmt = $mysqli->prepare("SELECT teamname , email,mobile, gender, age FROM members where email='$email'");
-                while ( $stmt-> fetch() ) {
+        $stmt = $mysqli->prepare("SELECT teamname, email, gender, age FROM members WHERE email = '$email'");
+        if ($stmt->execute()) {
+            $stmt-> bind_result($token2,$token3,$token6,$token7);
+
+            while ( $stmt-> fetch() ) {
                $user["name"] = $token2;
                $user["email"] = $token3;
-               $user["mobile"] = $token5;
+               
                $user["gender"] = $token6;
                $user["age"] = $token7;
             }
-                $stmt-> bind_result($token2,$token3,$token5,$token6,$token7);
 
-            
-
-                 $response["error"] = FALSE;
-        $response["user"]["name"] = $token2;
-        $response["user"]["email"] = $token3;
-        $response["user"]["mobile"] = $token5;
+            $stmt->close();
+        // use is found
+        $response["error"] = FALSE;
+        $response["user"]["name"] = $user["name"];
+        $response["user"]["email"] = $user["email"];
+        $response["user"]["age"] = $user["age"];
+        $response["user"]["gender"] = $user["gender"];
+         echo json_encode($response);
        
-        $response["user"]["gender"] = $token6;
-         $response["user"]["age"] = $token7;
+    } else {
+        // user is not found with the credentials
+        $response["error"] = TRUE;
+        $response["error_msg"] = "Login credentials are wrong. Please try again!";
         echo json_encode($response);
-        
+    }
         
     } else {
-      echo $email;
-      echo "email here";
         // Login failed 
-         header('Location: ../index.php?error=1');
+        header('Location: ../index.php?error=1');
     }
 } else {
     // The correct POST variables were not sent to this page. 
     echo 'Invalid Request';
 }
-}
-?>
-<?php
-include("head.php")
-?>
-<script type="text/JavaScript" src="js/sha512.js"></script>
-<script type="text/JavaScript" src="js/forms.js"></script>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<style type="text/css">
-.form-gradient .font-small {
-font-size: 0.8rem; }
-.form-gradient .header {
-border-top-left-radius: .3rem;
-border-top-right-radius: .3rem; }
-.form-gradient input[type=text]:focus:not([readonly]) {
-border-bottom: 1px solid #fd9267;
--webkit-box-shadow: 0 1px 0 0 #fd9267;
-box-shadow: 0 1px 0 0 #fd9267; }
-.form-gradient input[type=text]:focus:not([readonly]) + label {
-color: #4f4f4f; }
-.form-gradient input[type=password]:focus:not([readonly]) {
-border-bottom: 1px solid #fd9267;
--webkit-box-shadow: 0 1px 0 0 #fd9267;
-box-shadow: 0 1px 0 0 #fd9267; }
-.form-gradient input[type=password]:focus:not([readonly]) + label {
-color: #4f4f4f; }
-</style>
-<main>
-  
-  <div class="container-fluid text-center">
-    <!--Card-->
-    <div class="card card-cascade wider reverse my-4 pb-5">
-      <!--Card image-->
-      
-      
-      <!--/Card image-->
-      <!--Card content-->
-      
-      <?php
-      if (isset($_GET['error'])) {
-      echo '<p class="error">Error Logging In!</p>';
-      }
-      ?>
-      
-      <section class="form-gradient d-flex" style="align-self: center;" >
-        <!--Form with header-->
-        <div class="card ">
-          <!--Header-->
-          <div class="header pt-1 aqua-gradient">
-            <div class="row d-flex justify-content-center">
-              <h3 class="white-text mb-3 pt-3 font-weight-bold">Log in</h3>
-            </div>
-            
-          </div>
-          <!--Header-->
-          <div class="card-body mx-5 mt-5">
-            
-            <?php echo '<p class="blue-text">Round ' . $round . '   </p>'; ?>
-            <!--Body-->
-            <form action=<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?> method="post" name="login_form">
-              <div class="md-form ">
-                <input type="text" id="email" class="form-control" name="email">
-                <label for="Form-email3">Your email</label>
-              </div>
-              <div class="md-form pb-1 pb-md-3">
-                <input type="password" class="form-control" name="password" id="password">
-                <label for="Form-pass3">Your password</label>
-                
-              </div>
-              <!--Grid row-->
-              <div class="row d-flex align-items-center mb-4">
-                <!--Grid column-->
-                <div class="col-md-1 col-md-5 d-flex align-items-start">
-                  <div class="text-center">
-                    <button type="button" class="btn btn-grey btn-primary z-depth-1a "  onclick="formhash(this.form, this.form.password)" >Log in</button>
-                  </div>
-                </div>
-              </form>
-              <!--Grid column-->
-              <!--Grid column-->
-              <div class="col-md-7">
-                <p class="font-small grey-text d-flex justify-content-end mt-3">Don't have an account? <a href="register.php" class="dark-grey-text ml-1 font-weight-bold"> Sign up</a></p>
-              </div>
-              <!--Grid column-->
-            </div>
-            <!--Grid row-->
-            <?php
-            if (login_check($mysqli) == true) {
-            echo '<p class="blue-text">Currently logged ' . $logged . ' as ' . htmlentities($_SESSION['teamname']) . '.  <a class="btn btn-secondary btn-grey waves-effect purple-gradient waves-light ">Continue</a>
-          </p>';
-          
-          echo '<p class="blue-text">Do you want to change user? &nbsp<a class="btn btn-default purple-gradient blue-text btn-grey waves-effect waves-light" href="includes/logout.php">&nbsp Logout &nbsp </a>.</p>';
-          } else {
-          echo '<p class="blue-text">Currently logged ' . $logged . '.</p>';
-          echo '<p class="blue-text">If you don\'t have a login, please <a class="btn purple-gradient btn-default blue-text btn-grey waves-effect waves-light" href="register.php">Register</a>.</p>';
-          }
-          ?>
-        </div>
-        
-        <!--/Form with header-->
-      </section>
-      
-      
-      
-      <!-- <a class="btn btn-secondary btn-lg waves-effect waves-light">Secondary button</a>
-      <a class="btn btn-default btn-lg waves-effect waves-light">Default button</a> -->
-    </div>
-    <!--/.Card content-->
-  </div>
-  <!--/.Card-->
-  
-</div>
-</main>
-<?php
-include("foot.php")
+
 ?>
