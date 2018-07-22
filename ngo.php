2937969e859364632062989d5c9fdab99bc18a9f
smit706 <38158379+smit706@users.noreply.github.com>
2018-07-22 11:03:27 +0530

    ngo updated

--- ngo.php
+++ ngo.php
@@ -3,50 +3,55 @@ include_once 'includes/db_connect.php';
 include_once 'includes/functions.php';
 sec_session_start();
 $status=$_SESSION['type'];
-if (login_check($mysqli) == true) {
-  if ($status != "ngo") {
-                     # code...
-    header('Location: redirect.php');
+// if (login_check($mysqli) == true) {
+//   if ($status != "ngo") {
+//                      # code...
+//     header('Location: redirect.php');
                
                
-}
+// }
 
-}
-else {
-  # code...
-  header('Location: redirect.php');
-}
+// }
+// else {
+//   # code...
+//   header('Location: redirect.php');
+// }
 ?>
 <?php
 include("head.php")
 ?>
 <!--Main layout-->
 <main>
+
 <body>
 <form>
-  <center>
-    <input type="text" placeholder="search" />&nbsp;<button style="background-color: white; border: none; cursor: pointer;"><i class="fas fa-search" style="font-size: 1.3em"></i></button>
-  </center>
-  <br><br><br>
-  <!-- Card -->
-<div class="card">
-<!-- Card content -->
-  <div class="card-body">
-  	 <div class="form-group">
-    <label for="exampleFormControlTextarea2">Ask questions.....</label>
-    <textarea class="form-control rounded-0" id="exampleFormControlTextarea2" rows="3" placeholder="Question...."></textarea>
-    <button type="button" style="background-color: white;"><i class="far fa-comment-alt"></i></button>
-    <input type="text" placeholder="comments..." id="1">
-    <button type="button" class="btn btn-grey btn-primary z-depth-1a">Submit</button>
-	</div>
-    <!-- Button -->
-    <a href="#" class="btn btn-primary" style="position: relative; left: 75em;">Post</a>
-
-  </div>
-
-</div>
+ 
 <!-- Card -->
  
+<div class="card" style="width: 50%;">
+	<div class="header peach-gradient">
+		<div class="row d-flex justify-content-center">
+                <h3 class="white-text mb-0 py-5 font-weight-bold">Set time and date</h3>
+        </div>
+    </div>
+    <div class="card-body">
+        <!-- Default input -->
+		<label for="date" class="">Enter date:</label>
+		<input type="text" id="date" class="form-control" placeholder="DD/MM/YY" style="width: 8em;">
+		<br>
+		<label for="time" class="">Enter time(24 hr format):</label>
+		<input type="text" id="time" class="form-control" placeholder="HH/MM" style="width: 8em;">
+		<br>
+		<div class="form-group">
+    	<label for="address">Enter address:</label>
+    	<textarea class="form-control rounded-0" id="address" rows="3" placeholder="Address..."></textarea>
+		</div>
+		<div class="md-form">
+  			<button style="cursor: pointer;" class="btn btn-cyan">Submit</button>
+        </div>
+    </div>
+
+</div>
 </form>
 <br>
 </main>
