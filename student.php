8b982560bc55a6c3d33b0cce9f824f4a7e97805f
vrajpatel2312 <vraj.vup@gmail.com>
2018-07-21 20:16:37 -0400

    .

--- student.php
+++ student.php
@@ -15,46 +15,105 @@ $stmt->fetch();
 <?php
 include("head.php")
 ?>
+<style type="text/css">
+  .form-gradient .header {
+  border-top-left-radius: .3rem;
+  border-top-right-radius: .3rem; }
+
+.form-gradient input[type=text]:focus:not([readonly]) {
+  border-bottom: 1px solid #fd9267;
+  box-shadow: 0 1px 0 0 #fd9267; }
+
+.form-gradient input[type=text]:focus:not([readonly]) + label {
+  color: #4f4f4f; }
+
+.form-gradient textarea.md-textarea:focus:not([readonly]) {
+    border-bottom: 1px solid #fd9267;
+    box-shadow: 0 1px 0 0 #fd9267; }
+
+
+.form-dark .md-form label {
+  color: #fff; }
+
+.form-dark input[type=text]:focus:not([readonly]) {
+  border-bottom: 1px solid #00C851;
+  -webkit-box-shadow: 0 1px 0 0 #00C851;
+  box-shadow: 0 1px 0 0 #00C851; }
+
+.form-dark input[type=text]:focus:not([readonly]) + label {
+  color: #fff; }
+
+.form-dark textarea.md-textarea:focus:not([readonly]) {
+    border-bottom: 1px solid #00C851;
+    box-shadow: 0 1px 0 0 #00C851;
+    color: #fff; }
+
+.form-dark textarea.md-textarea  {
+    color: #fff;
+}
+
+.form-dark .form-control, .form-dark .form-control:focus {
+    color: #fff;
+}
+</style>
 <!--Main layout-->
+
 <main>
-<body>
-<form>
   <center>
-    <input type="text" placeholder="search" />&nbsp;<button style="background-color: white; border: none; cursor: pointer;"><i class="fas fa-search" style="font-size: 1.3em"></i></button>
-  </center>
-  <br><br><br>
-<!-- Card -->
-<div class="card">
+<!-- Section: form gradient -->
+<section class="form-gradient mb-5">
+
+    <!--Form with header-->
+    <div style="width: 40%" class="card">
+
+        <!--Header-->
+        <div class="header peach-gradient">
+
+            <div class="row d-flex justify-content-center">
+                <h3 class="white-text mb-0 py-5 font-weight-bold">Ask a Question</h3>
+            </div>
+
+        </div>
+        <!--Header-->
+
+        <div class="card-body mx-4">
+
+            <div class="md-form">
+                <i class="fa fa-tag prefix grey-text float-left"></i>
+                <input type="text" id="form106" class="form-control">
+                <label for="form106">Subject</label>
+            </div>
+
+            <div class="md-form">
+                <i class="fa fa-pencil prefix grey-text"></i>
+                <textarea type="text" id="form107" class="md-textarea form-control" rows="5"></textarea>
+                <label for="form107">Your message</label>
+            </div>
 
 
-  <!-- Card content -->
-  <div class="card-body">
-     <div class="form-group">
-    <label for="exampleFormControlTextarea2">Ask questions.....</label>
-    <textarea class="form-control rounded-0" id="exampleFormControlTextarea2" rows="3" placeholder="Question...."></textarea>
-    <button type="button" style="background-color: white;"><i class="far fa-comment-alt"></i></button>
-    <input type="text" placeholder="comments..." id="1">
-    <!-- <button type="button" class="btn-cyan" style="cursor: pointer;">Submit</button> -->
-    <button type="button" class="btn btn-grey btn-primary z-depth-1a">Submit</button>
-</div>
-    <!-- Button -->
-    <a href="#" class="btn btn-primary" style="position: relative; left: 75em;">Post</a>
+            <!--Grid row-->
+            <div class="row d-flex align-items-center mb-3 mt-4">
 
-  </div>
+                <!--Grid column-->
+                <div class="col-md-12">
+                    <div class="text-center">
+                        <button type="button" class="btn btn-grey btn-rounded z-depth-1a">Send</button>
+                    </div>
+                </div>
+                <!--Grid column-->
 
-</div>
-<!-- Card -->
+            </div>
+            <!--Grid row-->
+        </div>
 
- <!--  <div class="form-group">
-    <label for="exampleFormControlTextarea2">Ask questions.....</label>
-    <textarea class="form-control rounded-0" id="exampleFormControlTextarea2" rows="3" placeholder="Question...."></textarea>
-    <button type="button" style="background-color: white; position: absolute; top: 20em;"><i class="far fa-comment-alt"></i></button>
-    <input type="text" placeholder="comments..." id="1" style="position: absolute;top: 20em; left: 7em;">
-    <button type="button" class="btn-cyan" style="position: absolute;top: 20em;left: 19em; cursor: pointer;">Submit</button>
-    <button type="button" class="btn btn-grey btn-primary z-depth-1a" style="position: absolute; top: 24em; left: 88em;">Post</button>
-</div> -->
-</form>
-<br>
+    </div>
+    <!--/Form with header-->
+
+</section>
+</center>
+<!-- Section: form gradient -->
+<!-- Section: form dark -->
+<!-- Section: form dark -->
 </main>
 <?php
 include("foot.php")
