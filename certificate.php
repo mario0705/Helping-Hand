71416a40999d3c5174c93e5891ee9a53e1fd7948
smit706 <38158379+smit706@users.noreply.github.com>
2018-07-23 08:57:12 +0530

    certificate done

--- certificate.php
+++ certificate.php
@@ -25,9 +25,10 @@ include("head.php")
 <form>
 <!-- Card -->
 
-<!-- <div style="background-image: url(assets/image/logo.png); background-size: 50em; background-repeat: no-repeat; z-index: 10; opacity: 0.5; color: black;"> -->
+<div style="background-image: url(assets/image/logo3.jpg); background-repeat: no-repeat; background-color: rgba(0,0,0,0.2); width: 800px; height: 600px;">
 <div style="width:800px; height:600px; padding:20px; text-align:center; border: 10px solid #787878;">
 <div style="width:750px; height:550px; padding:20px; text-align:center; border: 5px solid #787878">
+	
        <span style="font-size:50px; font-weight:bold">Certificate of Completion</span>
        <br><br>
        <span style="font-size:25px"><i>This is to certify that</i></span>
@@ -35,11 +36,13 @@ include("head.php")
        <span style="font-size:30px"><b>$student.getFullName()</b></span><br/><br/>
        <span style="font-size:25px"><i>has completed the required task</i></span> <br/><br/>
        <span style="font-size:30px">$course.getName()</span> <br/><br/><br>
-       <img src="assets/image/logo.png" height="25%">
+   
+       <!-- <img src="assets/image/logo.png" height="25%"> -->
         <!-- <span style="font-size:20px">with score of <b>$grade.getPoints()%</b></span> <br/><br/><br/><br/> -->
        <!-- <span style="font-size:25px"><i>dated</i></span><br>
       #set ($dt = $DateFormatter.getFormattedDate($grade.getAwardDate(), "MMMM dd, yyyy"))
       <span style="font-size:30px">$dt</span> -->
+</div>
 </div>
 </div><br>
 <button style="cursor: pointer;" type="submit" class="btn btn-cyan">Print</button>
